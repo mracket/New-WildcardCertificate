daca0e7b198fc0a73455c6a680a9ec20ad4cad2b
Martin Therkelsen <martin@martins-mbp.home>
2017-12-27 17:58:09 +0100

    Initial code upload

---
+++ New-Certificate.ps1
@@ -0,0 +1,74 @@
+<#
+.Synopsis
+   Automate the creation of wildcard certificates
+.DESCRIPTION
+   This script can automate the creation of wildcard certificates from an internal PKI infrastructure.
+   The output PFX file will not have a password and it will be placed in the folder the PS1 script is located.
+   You will need to have the SSL.INI file in the same folder as this script and you will need to run the script as a domain users.
+   Use the function within this script by editing the line in the buttom.
+.PARAMETER Path
+  Path to where temporary files are stored
+.PARAMETER PFXPath
+  Path to where the PFX file is exported
+.PARAMETER CAName
+  Name of the Certificate authority    
+.EXAMPLE
+   New-WildcardCertificate
+.EXAMPLE
+   New-WildcardCertificate -Path C:\Temp -PFXPath "\\FILE01\Certificates" 
+.EXAMPLE
+   New-WildcardCertificate -Path C:\Temp -PFXPath "\\FILE01\Certificates" -CAName "DC01.Domain.Com\Domain-DC01-CA"
+#>
+Function New-WildcardCertificate {
+    [CmdletBinding()]
+    Param(
+      [Parameter(Mandatory=$False,Position=1)]
+       [string]$Path = "C:\Windows\Temp",
+       [Parameter(Mandatory=$False,Position=2)]
+       [string]$PFXPath	= ".",
+       [Parameter(Mandatory=$False,Position=3)]
+       [string]$CAName,
+       [Parameter(Mandatory=$False,Position=4)]
+       [string]$Password,
+       [Parameter(Mandatory=$False,Position=4)]
+       [string]$CertificateName
+    )
+    Begin {
+        $Domain = (Get-ItemProperty -path hklm:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters -Name Domain).Domain
+        If (!(Test-Path -Path $PSScriptRoot\ssl.ini)) {
+            Write-Host "You don't have the SSL.INI file that is required to run this script" -ForegroundColor Red
+            Break;
+        }
+        If (!(Test-Path -Path $Path)) {
+            New-Item -Path $Path -ItemType Directory
+        }
+        (Get-Content $PSScriptRoot\ssl.ini) | Foreach-Object {$_ -replace 'ServerFQDN',"$CertificateName"}  | Out-File .\Wildcard.ini
+    }
+    Process {
+        If ($CAName -eq "") {
+            Write-Verbose "Finding certificate authority"
+            $CA = New-Object -ComObject CertificateAuthority.Config
+            $CAName = $CA.GetConfig(0)            
+        }
+        Write-Verbose "Requesting certificate" 
+        & c:\windows\system32\certreq.exe –new "Wildcard.ini" "$Path\wildcard.req"
+        & c:\windows\system32\certreq.exe -config "$CAName" –submit "$Path\wildcard.req" "$Path\wildcard.cer"
+
+        Write-Verbose "Installing certificate" 
+        & c:\windows\system32\certreq.exe –accept "$Path\wildcard.cer"
+
+        Write-Verbose "Exporting certificate and private key"
+        $PFXPassword = ConvertTo-SecureString -String $Password -Force -AsPlainText
+        $cert = new-object security.cryptography.x509certificates.x509certificate2 -arg "$Path\wildcard.cer"
+        Get-item cert:\localmachine\my\$($cert.Thumbprint) | Export-PfxCertificate -FilePath "$PFXPath\Wildcard.pfx" -Password $PFXPassword 
+        Write-Verbose "Certificate successfully exportert to wildcard.pfx"
+    }   
+    End {
+        Write-Verbose "deleting exported certificat from computer store"
+        Remove-Item -Path cert:\localmachine\my\$($Cert.Thumbprint) -DeleteKey 
+        Remove-Item -Path $Path\wildcard.cer -Force
+        Remove-Item -Path $Path\wildcard.req -Force
+        Remove-Item -Path $Path\wildcard.rsp -Force
+    }
+}
+New-WildcardCertificate -Path C:\Install -PFXPath "\\MDT01\d$\Shares" -Password "Password1" -CertificateName "Server1.citrixlab.dk" -Verbose 
